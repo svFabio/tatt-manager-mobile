5ef2d3368e0972192a24b7e30090cc749d152d5e
svfabio <svfabio.andres@gmail.com>
2026-05-14 23:14:13 -0400

    chore: update appointments service routes and clean dependencies

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -6,7 +6,7 @@ interface AuthState {
   clearToken: () => void;
 }
 
-const DEV_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJzcGFAc2Ftc2FyYS5jb20iLCJyb2wiOiJBRE1JTiIsIm5lZ29jaW9JZCI6MSwiaWF0IjoxNzc2NDQyMjE4LCJleHAiOjE3NzcwNDcwMTh9.c31ije9LMpQ0hArYAM1x4SLfzJZRd4dk5c4YQtGVzEc";
+const DEV_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJzcGFAc2Ftc2FyYS5jb20iLCJyb2wiOiJBRE1JTiIsIm5lZ29jaW9JZCI6MSwiaWF0IjoxNzc4Nzg3NzY5LCJleHAiOjIwOTQzNjM3Njl9.1haBNvqfedxRB2UGDwYHIw0sYuTVMBbw5PZ9hJXar2w";
 
 export const useAuthStore = create<AuthState>((set) => ({
   token: DEV_TOKEN,
